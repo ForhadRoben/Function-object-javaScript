cd5d806e3794555bebba74010378847acfaca354
ForhadRobe <i.rforhad@gmail.com>
2022-08-10 22:30:38 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,9 @@
+/* var smartphone = { brand: "iPhone", model: "13" };
+// console.log(smartphone.brand);
+console.log(smartphone["brand"]); */
+function movie() {
+    return "Din-The day";
+    return "Poran";
+    return "Hawa";
+}
+console.log(movie());
